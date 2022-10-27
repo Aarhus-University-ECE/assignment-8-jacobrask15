f4d58cac716064102b1fa1531ddc46fe40a2a010
jacobrask15 <49784405+jacobrask15@users.noreply.github.com>
2022-10-27 22:28:54 +0200

    queues done

--- src/list_queue.c
+++ src/list_queue.c
@@ -1,22 +1,62 @@
 #include "list_queue.h"
-
+#include <assert.h>
 
 void init_queue(queue *q)
 {
   // Add your init_queue
+  q->size = 0;
+  q->front = NULL;
+  q->rear = NULL;
 }
 
 int empty(queue *q)
 {
-  // Add your empty function  
+  int x;
+
+  if (q->size == 0)
+  {
+    x = 1;
+  }
+  else
+  {
+    x = 0;
+  }
+
+  return x;
 }
 
 void enqueue(queue *q, int x)
 {
   // Add your enqueue function
+  qnode *new_node = (qnode *)malloc(sizeof(qnode)); /*Creates node that is being enqueued*/
+  new_node->data = x;                               /*inputs desired data in new node*/
+  new_node->next = NULL;                            /*links the new node to linked list*/
+
+  /*In the case of inserting first node in list*/
+  if (q->rear == NULL)
+  {
+    q->rear = new_node;
+    q->front = new_node;
+  }
+
+  else
+  {
+    q->rear->next = new_node; /*linsk new_node to list*/
+    q->rear = new_node;       /*updates  new_node to be rear*/
+  }
+
+  q->size++;
 }
 
 int dequeue(queue *q)
 {
   // Add your dequeue function
+  int x;                     /*output*/
+  assert(q->size > 0);       /*Cannot succesfully dequeue empty queue*/
+  x = q->front->data;        /*collects data from front*/
+  q->front = q->front->next; /*updates front to be node after the node we just dequeued*/
+
+  q->size = q->size - 1; /*list is 1 node smaller*/
+
+  return x;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,46 @@
-extern "C"{
-    #include "insertion_sort.h"
-    #include "linked_list.h"
+extern "C"
+{
+#include "insertion_sort.h"
+#include "linked_list.h"
+#include "list_queue.h"
 }
 
 // File for sandboxing and trying out code
 int main(int argc, char **argv)
 {
 
-    int testlist[] ={4,3,10,1,6};
-    linked_list* test = createLinkedList();
+    int testlist[] = {4, 3, 10, 1, 6};
+    linked_list *test = createLinkedList();
     for (int i = 0; i < 5; ++i)
     {
-        insertFront(createNode(testlist[5-i-1]), test);
+        insertFront(createNode(testlist[5 - i - 1]), test);
     }
-    
+
     printLL(test);
     sort(test);
     printLL(test);
-    
+
+    queue *test_queue = (queue *)malloc(sizeof(queue));
+    init_queue(test_queue);
+    int aa = empty(test_queue);
+    printf("\nSkal være 1:    %d", aa);
+
+    enqueue(test_queue, -5);
+    enqueue(test_queue, 10);
+    enqueue(test_queue, 0);
+    printf("\nFront skal være -5:    %d", test_queue->front->data);
+    printf("\nRear skal være 0:    %d", test_queue->rear->data);
+    int x = dequeue(test_queue);
+    printf("\nFront skal være 10:    %d", test_queue->front->data);
+    int y = dequeue(test_queue);
+    int z = dequeue(test_queue);
+    printf("\nSkal være -5:    %d", x);
+    printf("\nkal være 10:    %d", y);
+    printf("\nkal være 0:    %d", z);
+    printf("\nkal være 0:    %d", test_queue->size);
+
+    int a = empty(test_queue);
+    printf("\nSkal være 1:    %d", a);
+
     return 0;
 }
