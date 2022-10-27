a0c2f1a2c8e3d9f9f50392b06f75bf08588775ac
jacobrask15 <49784405+jacobrask15@users.noreply.github.com>
2022-10-27 18:55:45 +0200

    insertion sort and text answers done

--- src/insertion_sort.c
+++ src/insertion_sort.c
@@ -5,5 +5,39 @@
 
 void sort(linked_list *llPtr)
 {
- // Add your sort function here
+    node_t *ptr_to_current = llPtr->head->next;
+    node_t *ptr_to_pre = llPtr->head;
+    node_t *tracker = llPtr->head;
+    
+
+    while(ptr_to_current != NULL) 
+    {
+        /*In the case that node must be inserted as new head*/
+        if (ptr_to_current->data < ptr_to_pre->data && ptr_to_pre == llPtr->head) 
+        {
+            llPtr->head = swap(ptr_to_pre, ptr_to_current); /*New head must be the first in ll, which is returned by function*/
+            ptr_to_current = llPtr->head->next; /*now points to 2nd node in list*/
+            ptr_to_pre = llPtr->head; /*now points to 1st node in list*/
+        }
+
+        else if (ptr_to_current->data < ptr_to_pre->data) /*node needs to be sorted*/
+        {
+            /*tracker must be one node before pre*/
+            while (tracker->next != ptr_to_pre) 
+            {
+                tracker = tracker->next;
+            }
+
+            tracker->next = swap(ptr_to_pre, ptr_to_current); /*because tracker is one node before pre, the next of the tracker must point to the y of the funtion*/
+            ptr_to_pre = tracker; /*to compare to next data one down in list*/
+            tracker = llPtr->head; /*resets tracker to start*/
+        }
+
+        else
+        {
+            /*when list is sorted move currrent and previous the the right in list*/
+            ptr_to_current = ptr_to_current->next; 
+            ptr_to_pre = ptr_to_pre->next;
+        }
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 extern "C"{
-    // Add the header files required to run your main 
+    #include "insertion_sort.h"
+    #include "linked_list.h"
 }
 
 // File for sandboxing and trying out code
 int main(int argc, char **argv)
 {
+
+    int testlist[] ={4,3,10,1,6};
+    linked_list* test = createLinkedList();
+    for (int i = 0; i < 5; ++i)
+    {
+        insertFront(createNode(testlist[5-i-1]), test);
+    }
+    
+    printLL(test);
+    sort(test);
+    printLL(test);
+    
     return 0;
 }
