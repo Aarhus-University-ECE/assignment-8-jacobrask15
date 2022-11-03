dab0e9479064d1038e2d40ce42f55a4e23cc15cf
jacobrask15 <49784405+jacobrask15@users.noreply.github.com>
2022-11-03 07:10:33 +0100

    queue fixed

--- src/list_queue.c
+++ src/list_queue.c
@@ -33,29 +33,30 @@ void enqueue(queue *q, int x)
   new_node->next = NULL;                            /*links the new node to linked list*/
 
   /*In the case of inserting first node in list*/
-  if (q->rear == NULL)
+  if (q->size == 0)
   {
     q->rear = new_node;
     q->front = new_node;
   }
   else
   {
-    q->rear->next = new_node; /*linsk new_node to list*/
+    q->rear->next = new_node; /*links new_node to list*/
     q->rear = new_node;       /*updates  new_node to be rear*/
   }
 
   q->size++;
+  return;
 }
 
 int dequeue(queue *q)
 {
   // Add your dequeue function
-  int x;                     /*output*/
-  assert(q->size > 0);       /*Cannot succesfully dequeue empty queue*/
-  x = q->front->data;        /*collects data from front*/
-  q->front = q->front->next; /*updates front to be node after the node we just dequeued*/
+  int x; /*output*/
+  // assert(q->size > 0);       /*Cannot succesfully dequeue empty queue*/
+  x = q->front->data; /*collects data from front*/
 
-  q->size = q->size - 1; /*list is 1 node smaller*/
+  q->front = q->front->next; /*updates front to be node after the node we just dequeued*/
+  q->size = q->size - 1;     /*list is 1 node smaller*/
 
   return x;
 }
